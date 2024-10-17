d3bf471a65f5e925b35231ca536186b70006da2b
HamdaniDhiaeddine <hamdanidhia4@gmail.com>
2024-10-17 22:57:55 +0100

    cors and morgan

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 import express, { json } from 'express';
 import { connect } from 'mongoose';
+import cors from 'cors';
+import morgan from 'morgan';
 import userRoutes from './routes/user.js';
 import postRoutes from './routes/post.js';
 
 const app = express();
 
 // Middleware
-app.use(json());
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json()); // Handles JSON payloads
+app.use(express.urlencoded({ extended: true })); // Handles URL-encoded payloads
 
 // Routes
 app.use('/api/users', userRoutes);
@@ -19,6 +24,6 @@ connect('mongodb://localhost:27017/frApp', { useNewUrlParser: true, useUnifiedTo
 
 // Start the server
 const PORT = process.env.PORT || 2000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT,'0.0.0.0', () => {
+  console.log(`Server running on 0.0.0.0: ${PORT}`);
 });
